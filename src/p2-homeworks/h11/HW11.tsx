94ff9dc9cf48c53063258d29b2dfd45473880015
SRGREEN90 <you@example.com>
2022-02-21 16:05:24 +0300

    'HW11 SuperRange '

--- src/p2-homeworks/h11/HW11.tsx
+++ src/p2-homeworks/h11/HW11.tsx
@@ -3,8 +3,16 @@ import SuperRange from './common/c7-SuperRange/SuperRange'
 import SuperDoubleRange from './common/c8-SuperDoubleRange/SuperDoubleRange'
 
 function HW11() {
-    const [value1, setValue1] = useState(0)
-    const [value2, setValue2] = useState(100)
+    const [value1, setValue1] = useState(45)
+    const [value2, setValue2] = useState(50)
+
+
+
+  let onChangeRange = (value1: number, value2: number) => {
+      setValue1(value1)
+      setValue2 (value2)
+  }
+
 
     return (
         <div>
@@ -13,19 +21,26 @@ function HW11() {
 
             {/*should work (должно работать)*/}
             <div>
-                <span>{value1}</span>
+                <h2>{value1}</h2>
                 <SuperRange
+                    value={value1}
+                    onChangeRange={setValue1}
                     // сделать так чтоб value1 изменялось
                 />
             </div>
 
             <div>
-                <span>{value1}</span>
+                <h2>{value1}</h2>
                 <SuperDoubleRange
+                    onChangeRange={onChangeRange}
+                    value={[value1, value2]}
+                    step={0.01}
+                    disable={true}
                     // сделать так чтоб value1 и value2 изменялось
                 />
-                <span>{value2}</span>
+                <h2>{value2}</h2>
             </div>
+
 
             <hr/>
             {/*для личного творчества, могу проверить*/}
--- src/p2-homeworks/h11/common/c7-SuperRange/SuperRange.tsx
+++ src/p2-homeworks/h11/common/c7-SuperRange/SuperRange.tsx
@@ -1,5 +1,6 @@
-import React, {ChangeEvent, DetailedHTMLProps, InputHTMLAttributes} from 'react'
+import React, { DetailedHTMLProps, InputHTMLAttributes} from 'react'
 import s from './SuperRange.module.css'
+import {Slider} from "@mui/material";
 
 // тип пропсов обычного инпута
 type DefaultInputPropsType = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
@@ -7,34 +8,37 @@ type DefaultInputPropsType = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElem
 // здесь мы говорим что у нашего инпута будут такие же пропсы как у обычного инпута
 // (чтоб не писать value: string, onChange: ...; они уже все описаны в DefaultInputPropsType)
 type SuperRangePropsType = DefaultInputPropsType & { // и + ещё пропсы которых нет в стандартном инпуте
-    onChangeRange?: (value: number) => void
+    onChangeRange: (value: number) => void
+    value: number
 };
 
 const SuperRange: React.FC<SuperRangePropsType> = (
     {
-        type, // достаём и игнорируем чтоб нельзя было задать другой тип инпута
+
+
+        type, value,// достаём и игнорируем чтоб нельзя было задать другой тип инпута
         onChange, onChangeRange,
         className,
 
         ...restProps// все остальные пропсы попадут в объект restProps
     }
 ) => {
-    const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
-        onChange && onChange(e) // сохраняем старую функциональность
-
-        onChangeRange && onChangeRange(+e.currentTarget.value)
+    const onChangeCallback = (event: Event, value: number | number[], activeThumb: number) => {
+        if(!Array.isArray(value)){
+            onChangeRange && onChangeRange(value)
+        }
     }
+
 
     const finalRangeClassName = `${s.range} ${className ? className : ''}`
 
     return (
         <>
-            <input
-                type={'range'}
-                onChange={onChangeCallback}
+            <Slider
                 className={finalRangeClassName}
-
-                {...restProps} // отдаём инпуту остальные пропсы если они есть (value например там внутри)
+                value={value}
+                onChange={onChangeCallback}
+                valueLabelDisplay="auto"
             />
         </>
     )
