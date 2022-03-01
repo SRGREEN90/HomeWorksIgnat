ab8310e77c5239d9e71cb414667008f5cce1f77e
SRGREEN90 <you@example.com>
2022-03-01 18:08:00 +0300

    'HW13 RequestAPI.ts '

--- src/index.tsx
+++ src/index.tsx
@@ -5,15 +5,16 @@ import App from './p1-main/m1-ui/u1-app/App'
 import * as serviceWorker from './serviceWorker'
 import {Provider} from "react-redux";
 import store from "./p2-homeworks/h10/bll/store";
+import { HashRouter} from 'react-router-dom'
 
 
 ReactDOM.render(
-    // <BrowserRouter>
+    <HashRouter>
     <Provider store={store}>
         <App/>
     </Provider>
 
-    // </BrowserRouter>
+     </HashRouter>
     ,
     document.getElementById("root")
 )
